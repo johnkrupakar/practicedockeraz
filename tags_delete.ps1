b8f34ed11564465b53d4a750f98632d57b2bd5a0
johnkrupakar <67743005+johnkrupakar@users.noreply.github.com>
2023-03-09 00:08:40 -0800

    Added a powershell script to delete tags

---
+++ tags_delete.ps1
@@ -0,0 +1,15 @@
+# Set the ACR name and maximum number of tags to keep
+$acrName = "pacticetest"
+$acrRepository = "pacticetest/pacticetest"
+$maxTags = 1
+
+#Get the number of tags
+$tagsCount = (az acr repository show-tags --name $acrName --repository $acrRepository).length-2
+
+if ($tagsCount -gt $maxTags){
+	az acr repository show-tags -n $acrName --repository $acrRepository --orderby time_asc --top 1 --query "[0]" --output tsv | 
+	foreach { 
+		az acr repository delete --name $acrName --image ${acrRepository}:$_ --yes 
+	}
+}
+

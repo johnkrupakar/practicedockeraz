e56ed0b5a662a11931cf8a97e2e014b2ea7f13d0
johnkrupakar <67743005+johnkrupakar@users.noreply.github.com>
2023-03-13 01:40:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Docker Container !')
+  res.send('Hello Docker Container!')
 })
 
 app.listen(port, () => {

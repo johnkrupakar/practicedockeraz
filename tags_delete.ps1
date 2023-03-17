da4c4ebbc295db9e116102c72b4ef847dd10f32e
johnkrupakar <67743005+johnkrupakar@users.noreply.github.com>
2023-03-16 23:16:41 -0700

    Update tags_delete.ps1

--- tags_delete.ps1
+++ tags_delete.ps1
@@ -7,10 +7,10 @@ $acr_password = $envValues[3];
 
 sudo az login --username $az_username --password $az_password
 sudo az acr login -n pacticetest --username $acr_username --password $acr_password
-$acrName = "pacticetest"
+<#$acrName = "pacticetest"
 $acrRepository = "pacticetest/pacticetest"
 $maxTags = 10
 $tagsCount = (sudo az acr repository show-tags --name $acrName --repository $acrRepository).length-2
 if ($tagsCount -gt $maxTags){ sudo az acr repository show-tags -n $acrName --repository $acrRepository --orderby time_asc --top 1 --query "[0]" --output tsv | foreach { sudo az acr repository delete --name $acrName --image ${acrRepository}:$_ --yes } }
 # Delete the untagged manifest
-sudo az acr repository show-manifests -n $acrName --repository $acrRepository --query "[?tags[0]==null].digest" -o tsv | %{ az acr repository delete -n $acrName -t ${acrRepository}@$_  --yes } 
+sudo az acr repository show-manifests -n $acrName --repository $acrRepository --query "[?tags[0]==null].digest" -o tsv | %{ az acr repository delete -n $acrName -t ${acrRepository}@$_  --yes } #>
